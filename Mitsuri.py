8cacf21571369836fd82d8b6e02fd84dd866d8c2
VKIIDS <102248018+VKIIDS@users.noreply.github.com>
2023-02-25 19:49:34 +0300

    Update Mitsuri.py

--- Mitsuri.py
+++ Mitsuri.py
@@ -5,7 +5,6 @@ print ('''
 ██║╚██╔╝██║██║░░░██║░░░░╚═══██╗██║░░░██║██╔══██╗██║
 ██║░╚═╝░██║██║░░░██║░░░██████╔╝╚██████╔╝██║░░██║██║
 ╚═╝░░░░░╚═╝╚═╝░░░╚═╝░░░╚═════╝░░╚═════╝░╚═╝░░╚═╝╚═╝
-
     ''')
 print('What you want to use?')
 print('[+] Calculator')
@@ -18,6 +17,7 @@ print ('[+] Nmap scripts')
 print('[+] How to use')
 print('[+] How to install')
 print('[+] Contact me')
+print('[+] Hacker teams')
 print ('For example: Ip_info, Contact_me or more')
 a = input('What you wanna use from this script: ')
 if a == ('Calculator'):
@@ -39,11 +39,176 @@ if a == ('Calculator'):
          print(A - B)
     elif C == ('+'):
          print(A + B)
+if a == ('Hacker_teams'):
+    print('''
+T.Y.G Team              https://t.me/TYG_TEAM        Mr.Sami   https://github.com/mr-sami-x  
+    
+    
+    
+    
+    
+    
+    
+    ''')
+    print("What telegram info you wan't to see")
+    g = input()
+    if g ==('T.Y.G_Team'):
+        print('''
+
+████████╗░░░██╗░░░██╗░░░░██████╗░░░░░░░████████╗███████╗░█████╗░███╗░░░███╗
+╚══██╔══╝░░░╚██╗░██╔╝░░░██╔════╝░░░░░░░╚══██╔══╝██╔════╝██╔══██╗████╗░████║
+░░░██║░░░░░░░╚████╔╝░░░░██║░░██╗░█████╗░░░██║░░░█████╗░░███████║██╔████╔██║
+░░░██║░░░░░░░░╚██╔╝░░░░░██║░░╚██╗╚════╝░░░██║░░░██╔══╝░░██╔══██║██║╚██╔╝██║
+░░░██║░░░██╗░░░██║░░░██╗╚██████╔╝░░░░░░░░░██║░░░███████╗██║░░██║██║░╚═╝░██║
+░░░╚═╝░░░╚═╝░░░╚═╝░░░╚═╝░╚═════╝░░░░░░░░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝
+
+
+T.Y.G team was top 3 in the world of hacking.Leader of T.Y.G team Mr.Sami . Mr.Sami created so many tools, this tools can be in his github account:
+https://github.com/mr-sami-x
+The leader of the so-called T.Y.G - Team (known for helping KillNet, Hacktivist Of Garuda, Anonymous Al-Sury, STORMOUS, AnonyMous IslaMic, and a lot more), Mr Sami, is also associated with “Y.C.A” aka Yemen Cyber Army, pirates of Yemen, and The Yemeni Ghost. All have solid communities in Telegram with around 5 000 users in each channel/group.
+(text from Tesseract Intelegence)         
+To join T.Y.G Team you need good learn hacking, and contact Mr.Sami.
+P.s i want to to join T.Y.G Team, i learning hacking to become part of T.Y.G Team :)
+        ''')
+if a ==('Info'):
+    print('''
+Hello, i'm Okay, i'm creator of this tool.Please read the readme.md this help you navigate in this tool, and help you use this tool :).This tool help novices.Here been courses,books,tutorials,and more another.This tool help you learning hacking , offencive security, learn hacking tools, and somebody help you making scripts in python :).So ye, if you have questions tell me in telegram:
+@Neyaneste    
+ ''')
+if a == ('Contact_me'):
+     print('''
+If you wanna contact me, my user in telegram is:
+@Neyaneste
+My discord is:
+My name is okay#1064  
+Discord group:
+
+     
+     ''')
 if a == ('How_to_use'):
     print('''
     Name     Type                   Link                            Git
     Dmitry  Osint.      https://youtu.be/z2EUhV11QB4  https://github.com/jaygreig86/dmitry
     
     Nmap    Scanner     https://youtu.be/5MTZdN9TEO4  https://github.com/nmap/nmap   
+    
+    Burp    VulnScanner https://youtu.be/IWWYNDiwYOA  https://github.com/thehackingsage/burpsuite
+    
+    Sqlmap  Hacking     https://youtu.be/2YD4vygeghM  https://github.com/sqlmapproject/sqlmap
+    
+    Hydra   Bruiteforce https://youtu.be/d2kbH1mncLQ  https://github.com/vanhauser-thc/thc-hydra
+     
+ Metasploit Hacking     https://youtu.be/8lR27r8Y_ik  https://github.com/rapid7/metasploit-framework
+    
+ OWASP ZAP  VulnScanner https://youtu.be/Ycas8mskSZk  https://github.com/zaproxy/zaproxy
+    
+    
+    
+    
+    
+    
+    
+    ''')
+    print('What info of tool you wanna see ')
+    s = input()
+    if s == ('Nmap'):
+        print('''
+Nmap (Network Mapper) is a network scanner created by Gordon Lyon (also known by his pseudonym Fyodor Vaskovich). Nmap is used to discover hosts and services on a computer network by sending packets and analyzing the responses.Nmap provides a number of features for probing computer networks, including host discovery and service and operating system detection. These features are extensible by scripts that provide more advanced service detection, vulnerability detection, and other features. Nmap can adapt to network conditions including latency and congestion during a scan.
+Nmap started as a Linux utility and was ported to other systems including Windows, macOS, and BSD it is most popular on Linux, followed by Windows.
+Nmap was created in September 1997.He was writen by C,C++,Python,Lua.Nmap can audit the security of a device or firewall by identifying the network connections which can be made to, or through it.
+Identifying open ports on a target host in preparation for auditing.
+Network inventory, network mapping, maintenance and asset management.
+Auditing the security of a network by identifying new servers.
+Generating traffic to hosts on a network, response analysis and response time measurement.
+Finding and exploiting vulnerabilities in a network.
+DNS queries and subdomain search.If you wanna install this tool, you need write:sudo apt get-install nmap.In video what i pin in link, you unknow so many info about this scanner tool, and how to use it.
+P.s You can use this playlist for unknow how to perfectly use nmap:
+https://www.youtube.com/watch?v=5MTZdN9TEO4&list=PLBf0hzazHTGPx4_jgz6wOJoj4cijSv1wW
+        #
+       ''')
+    if s == ('Dmitry'):
+         print('''
+DMitry is a UNIX/(GNU)Linux command line application written in C. DMitry can find possible subdomains, email addresses, uptime information, perform tcp port scan, whois lookups, and more.
+DMitry has a base functionality with the ability to add new functions. Basic functionality of DMitry allows for information to be gathered about a target host from a sim- ple whois lookup on the target to UpTime reports and TCP portscans.
 
+The application is considered a tool to assist in informa- tion gathering when information is required quickly by removing the need to enter multiple commands and the timely process of searching through data from multiple sources.
+
+To get straight into DMitry without reading this document, you can initially type "dmitry target", this will perform the majority of functions on the target.  
+If you wanna install this tool write in console sudo apt-get dmitry or find tut in git repo:
+https://github.com/jaygreig86/dmitry
+P.s you can find tut in this link :)
+https://youtu.be/z2EUhV11QB4
+                 ''')
+    if s == ('Sqlmap'):
+        print('''
+Sqlmap is an open source penetration testing tool that automates the process of detecting and exploiting SQL injection flaws and taking over of database servers. It comes with a powerful detection engine, many niche features for the ultimate penetration tester, and a broad range of switches including database fingerprinting, over data fetching from the database, accessing the underlying file system, and executing commands on the operating system via out-of-band connections.
+Attackers can use the tool to get into a database, and potentially the server
+Sqlmap is capable of providing a sql shell into the database - allowing an attacker to potentially execute any arbitrary sql command.
+Moreover, sqlmap also has an option to provide the attacker with an OS shell, with which the attacker can execute any arbitrary OS commands (Sql Injection leading to Command Injection)
+Sqlmap will also try to crack user passwords when it finds hashes, using dictionary attacks - so attackers can even use this tool to get your passwords
+Defenders can use sqlmap for penetration testing of their web applications, servers, and databases.
+Use the tool to crack week passwords, assess whether the database is run with restrictive privileges, and to detect any potential injection holes in the application
+If you wanna install this tool write in console:
+git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git sqlmap-dev
+Or just see tut in git repo:
+https://github.com/sqlmapproject/sqlmap
+This 3 links xan help you to unknow how to use this tool for maksimum:
+https://youtu.be/d-pcM0p2lt8
+https://youtu.be/nVj8MUKkzQk        
+https://youtu.be/kY_7GXoXGjg        
+                ''')
+    if s == ('Hydra'):
+        print('''
+Hydra (or THC Hydra) is a parallelized network login cracker built in various operating systems like Kali Linux, Parrot and other major penetration testing environments. Hydra works by using different approaches to perform brute-force attacks in order to guess the right username and password combination. Hydra is commonly used by penetration testers together with a set of programmes like crunch, cupp etc, which are used to generate wordlists. Hydra is then used to test the attacks using the wordlists that these programmes created.
+If you wanna install Hydra just read a tut from git repo:
+https://github.com/vanhauser-thc/thc-hydra
+You can unknow how to use hydra by thus links:
+https://youtu.be/-gE4leMl5Gg
+https://youtu.be/D2-Eq12hZ1o
+https://youtu.be/YrMNih3Z-4Y        
+https://youtu.be/rGQ-hekAjJ8        
+
+        ''')
+    if s == ('Burp'):
+        print('''
+Burp or Burp Suite is a set of tools used for penetration testing of web applications. It is developed by the company named Portswigger, which is also the alias of its founder Dafydd Stuttard. BurpSuite aims to be an all in one set of tools and its capabilities can be enhanced by installing add-ons that are called BApps.
+It is the most popular tool among professional web app security researchers and bug bounty hunters. Its ease of use makes it a more suitable choice over free alternatives like OWASP ZAP. Burp Suite is available as a community edition which is free, professional edition that costs $399/year and an enterprise edition that costs $3999/Year. This article gives a brief introduction to the tools offered by BurpSuite. If you are a complete beginner in Web Application Pentest/Web App Hacking/Bug Bounty, we would recommend you to just read through without thinking too much about a term.
+If you wanna install it, see tut in this link:
+https://portswigger.net/burp/documentation/desktop/getting-started/download-and-install
+Github help you with plugins and more:
+https://github.com/thehackingsage/burpsuite
+This tutorials can help you learn how to work with burp suite:
+https://youtu.be/3HW4cK-L4io
+https://youtu.be/IWWYNDiwYOA
+https://youtu.be/mK3Hr6ktgNg
+        ''')
+    if s == ('Metasploit'):
+        print('''
+The Metasploit Project is a computer security project that provides information about security vulnerabilities and aids in penetration testing and IDS signature development. It is owned by Boston, Massachusetts-based security company Rapid7.
+Its best-known sub-project is the open-source Metasploit Framework, a tool for developing and executing exploit code against a remote target machine. Other important sub-projects include the Opcode Database, shellcode archive and related research.
+
+The Metasploit Project includes anti-forensic and evasion tools, some of which are built into the Metasploit Framework. Metasploit is pre-installed in the Kali Linux operating system.
+    
+If you wanna install Metasploit, use this guide:
+https://docs.rapid7.com/metasploit/installing-the-metasploit-framework/
+    
+P.s this playlist help you with learning metasploit:
+https://youtube.com/playlist?list=PLBf0hzazHTGN31ZPTzBbk70bohTYT7HSm    
+
+    ''')
+    if s == ('OWASP_ZAP'):
+        print('''
+OWASP ZAP (short for Zed Attack Proxy) is an open-source web application security scanner. It is intended to be used by both those new to application security as well as professional penetration testers.
+It is one of the most active Open Web Application Security Project (OWASP) projects and has been given Flagship status.
+
+When used as a proxy server it allows the user to manipulate all of the traffic that passes through it, including traffic using HTTPS.
+
+It can also run in a daemon mode which is then controlled via a REST API.
+
+ZAP was added to the ThoughtWorks Technology Radar on May 30, 2015 in the Trial ring.
+
+ZAP was originally forked from Paros, another pentesting proxy. Simon Bennetts, the project lead, stated in 2014 that only 20% of ZAP's source code was still from Paros.    
+    
+If you want to unknow how to install and how to install OWASP ZAP this link can help you:
+ https://youtu.be/Ycas8mskSZk
     ''')
